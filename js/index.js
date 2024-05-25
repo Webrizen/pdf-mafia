40007753c921a675477031115b97bc1811493f6c
WebRizen <webrizen@gmail.com>
2024-05-25 10:17:48 +0530

    "Added PDF container and styling, updated file input fields to accept PDFs, and implemented PDF display and generation functionality."

--- js/index.js
+++ js/index.js
@@ -4,11 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
   currentDateInput.value = currentDate;
 });
 document.addEventListener("DOMContentLoaded", function () {
-  var currentDateInput = document.getElementById("latestDate");
-  var latestDate = new Date().toLocaleDateString();
-  currentDateInput.value = latestDate;
-});
-document.addEventListener("DOMContentLoaded", function () {
   var currentDateInput = document.getElementById("thirdDateBox");
   var thirdDateBox = new Date().toLocaleDateString();
   currentDateInput.value = thirdDateBox;
